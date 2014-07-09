56762f060244e707283cd59751b234616c420814
Ali Baharev <ali.baharev@gmail.com>
2014-07-09 19:38:35 +0200

    Extracting the sparsity pattern and the suffixes from the .nl file

---
+++ ampl_parser.py
@@ -0,0 +1,103 @@
+import fileinput
+import numpy as np
+
+def check_if_text_format(first_line):
+    if not first_line.startswith('g'):
+        print 'First line: \'{}\''.format(first_line)
+        raise RuntimeError('only ASCII format files can be parsed (specify the g flag to AMPL)')
+
+def extract_length(line):
+    # 'k42 <arbitrary text>' -> 42
+    l = line.split()
+    return int(l[0][1:])
+
+def extract_id_len(line):
+    # 'J5 2 <arbitrary text>' -> id=5, len=2
+    l = line.split()
+    return int(l[0][1:]), int(l[1])
+
+def extract_id_len_name(line):
+    # 'S1 20 blockid <arbitrary text>' -> id=1, len=20, name=blockid
+    l = line.split()
+    return int(l[0][1:]), int(l[1]), l[2]
+
+def extract_key_value(iterable, length):
+    # '3 42' -> 3, 42
+    for _ in xrange(length):
+        key, value = next(iterable).strip().split()
+        yield int(key)
+        yield int(value)
+
+def numpy_key_value(iterable, length):
+    arr = np.fromiter(extract_key_value(iterable, length), np.int32)
+    return arr.reshape(arr.size//2, 2)
+
+class J_segment():
+    # J5 2
+    # 1 1   ->  5: [1, 3], linearity info currently discarded
+    # 3 1
+    def __init__(self):
+        self.jacobian = { }
+    def __call__(self, iterable, line):
+        row, length = extract_id_len(line)
+        vars_in_row = numpy_key_value(iterable, length)[:,0] # keys only
+        self.jacobian.update({row : np.array(vars_in_row)})
+
+class k_segment():
+    def __call__(self, iterable, line):
+        length = extract_length(line)
+        self.col_len = np.fromiter(iterable, np.int32, length)
+
+class S_segment():
+    def __init__(self, nrows, ncols):
+        self.nrows = nrows
+        self.ncols = ncols
+        self.rows = { }
+        self.cols = { }
+    def __call__(self, iterable, line):
+        kind, length, name = extract_id_len_name(line)
+        key_value = numpy_key_value(iterable, length)
+        keys = key_value[:,0]
+        assert np.all(keys==np.arange(keys.size)),'Unexpected input:\n%s' % keys
+        # magic numbers from AMPL doc
+        suffixes = { 0: self.cols, 1: self.rows }.get(kind, { })
+        suffixes.update( {name: np.array(key_value[:,1]) } )
+
+def extract_problem_info(second_line):
+    data = second_line.split()
+    nrows = data[1] # These magic numbers come from the AMPL doc
+    ncols = data[0]
+    neqns = data[4]
+    if nrows!=neqns:
+        print 'WARNING: Not all constraints are equality constraints!'
+    return nrows, ncols
+
+def parse(f):
+    check_if_text_format(next(f))
+    nrows, ncols = extract_problem_info(next(f).strip())
+    funcs = { 'J': J_segment(),
+              'k': k_segment(),
+              'S': S_segment(nrows, ncols) }
+    for line in f:
+        first_char = line[0]
+        func = funcs.get(first_char)
+        if func:
+            func(f, line)
+    print 'Finished reading the nl file'
+    print 'k segment'
+    print funcs['k'].col_len
+    print funcs['J'].jacobian
+    print funcs['S'].rows
+    print funcs['S'].cols
+    return funcs['J'].jacobian, funcs['k'].col_len
+
+def read_flattened_ampl(filename):
+    try:
+        f = fileinput.input(filename, mode='r')
+        return parse(f)
+    finally:
+        print 'Read', f.lineno(), 'lines'
+        f.close()
+
+if __name__ == '__main__':
+    read_flattened_ampl('/home/ali/pyton-ws/sparse-matrix-computations/nl/block.nl')
---
+++ nl/block.nl
@@ -0,0 +1,86 @@
+g7 0 1 0 7 20131018 0 1	# problem suffixTest
+ 7 7 0 0 7	# vars, constraints, objectives, ranges, eqns
+ 0 0	# nonlinear constraints, objectives
+ 0 0	# network constraints: nonlinear, linear
+ 0 0 0	# nonlinear vars in constraints, objectives, both
+ 0 0 0 1	# linear network variables; functions; arith, flags
+ 0 0 0 0 0	# discrete variables: binary, integer, nonlinear (b,c,o)
+ 16 0	# nonzeros in Jacobian, gradients
+ 4 2	# max name lengths: constraints, variables
+ 0 0 0 0 0	# common exprs: b,c,o,c1,o1
+S0 7 blockid
+0 3
+1 4
+2 2
+3 4
+4 2
+5 1
+6 1
+S1 7 blockid
+0 3
+1 3
+2 4
+3 4
+4 1
+5 1
+6 2
+C0	#con0
+n0
+C1	#con1
+n0
+C2	#con2
+n0
+C3	#con3
+n0
+C4	#con4
+n0
+C5	#con5
+n0
+C6	#con6
+n0
+r	#7 ranges (rhs's)
+4 0
+4 0
+4 0
+4 0
+4 0
+4 0
+4 0
+b	#7 bounds (on variables)
+3
+3
+3
+3
+3
+3
+3
+k6	#intermediate Jacobian column lengths
+2
+5
+7
+9
+12
+15
+J0 2
+0 1
+2 1
+J1 2
+2 1
+4 1
+J2 3
+4 1
+5 1
+6 1
+J3 2
+4 1
+5 1
+J4 3
+1 1
+3 1
+5 1
+J5 2
+1 1
+3 1
+J6 2
+0 1
+1 1
