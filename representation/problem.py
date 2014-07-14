424f3b0bd63f44cad3e720188696c55148d4e106
Ali Baharev <ali.baharev@gmail.com>
2014-07-14 17:24:16 +0200

    Making the assumptions concerning the CSEs explicit

--- representation/problem.py
+++ representation/problem.py
@@ -1,5 +1,6 @@
 import networkx as nx
 import weakref
+import nodes
 from nodes.attributes import NodeAttr
 from networkx.algorithms.dag import ancestors, topological_sort
 
@@ -10,7 +11,8 @@ def iter_attr(G, nbunch, name):
 def is_leaf(dag, node_id):
     return len(dag.pred[node_id])==0
 
-# FIXME Must respect children order
+# FIXME Respect children order! Wrap: add_edge, remove_node, remove_edge,
+#                                     reverse_edge
 def reparent(dag, var_num, node_id):
     # delete node_id and connect all children to var_num, with edge dict
     out_edges = dag.edge[node_id]
@@ -44,11 +46,18 @@ class Problem:
         for node_id, d in self.dag.nodes_iter(data=True):
             d[NodeAttr.dag] = weakref.ref(self.dag)
             d[NodeAttr.type].setup(node_id, d, self)
+
+        # remove bogus named var aliasing
         assert_var_num_equals_node_id_for_named_vars(self.dag, self.var_num_name)
-        # removed named vars, we know they are nodes 0:nvars
+        # remove named vars from var_node_ids, we asserted just above that
+        # they are nodes 0:nvars
         self.var_node_ids.difference_update( xrange(self.nvars) )
-        # the rest assumes that named var nodes are no longer in var_node_ids
+        # the rest assumes that named var nodes are no longer in var_node_ids!
         self.remove_var_aliases()
+
+        # nl2dag erroneously turns defined vars into constraints
+        self.reconstruct_CSEs()
+
         self.print_constraints()
 
     def remove_var_aliases(self):
@@ -65,6 +74,14 @@ class Problem:
             if var_num < nvars: # true if referencing a named var; false for CSE
                 var_aliases[node_id] = var_num
         return var_aliases
+
+    def reconstruct_CSEs(self):
+        cons_ends = self.get_cons_wo_name()
+        assert_CSE_defining_constraints(self.dag, cons_ends, self.var_num_name)
+
+    def get_cons_wo_name(self):
+        return [ n for n in self.con_ends_num   \
+                         if self.con_ends_num[n] not in self.con_num_name ]
 
     def print_constraints(self):
         print 'Constraint dependencies\n'
@@ -109,3 +126,30 @@ def assert_var_num_equals_node_id_for_named_vars(dag, var_num_name):
         var_num_on_node = d[NodeAttr.var_num]
         assert var_num_on_node==var_num, 'var_num on node %d, expected %d; %s' % \
                                          (var_num_on_node, var_num, d)
+
+# FIXME When reversing edge, the rhs should be changed accordingly
+#       lambda * <var node> + <some sum> + d = bounds
+#       <var node> = (-1/lambda) * ( <some sum> + d - bounds)
+#       Apparently: reverse edge, update multiplier to -1/lambda,
+#       d := d - bounds, delete bounds, and evaluate sum node as before
+def assert_CSE_defining_constraints(dag, con_ends, named_vars):
+    # A constraint (sum) node immediately followed by an unnamed var node; with
+    # an edge from the var node to the sum node; and the rhs of the constraint
+    # is a real number, not an interval.
+    # Basically: lambda * <var node> + <some sum> + d = bounds
+    # <N> c 20 145
+    # <145> b [0,0]: +
+    # <146> V 20
+    # <E> 145 146 -1
+    for n in con_ends:
+        d = dag.node[n]
+        assert d[NodeAttr.type]==nodes.sum_node,'expected a sum_node, found: %s' % d
+        assert NodeAttr.bounds in d,'Should have bounds, node: %s' % d
+        lb, ub = d[NodeAttr.bounds]
+        assert lb==ub,'rhs expected to be a constant, node: %s' % d
+        assert n+1 in dag,'expected a var node; not CSE defining constraint: %s' % d
+        def_var = dag.node[n+1]
+        assert def_var[NodeAttr.type]==nodes.var_node, \
+                                 'expected a var_node, found: %s' % def_var
+        assert n+1 not in named_vars,'expected an unnamed var, found %s' % def_var
+        assert n in dag.edge[n+1],'Nodes not connected:\n %s \n %s'%(d,def_var)
