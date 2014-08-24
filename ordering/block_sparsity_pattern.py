9009c039c22959359e57caf3f55df72141a06eba
baharev <ali.baharev@gmail.com>
2014-08-24 02:07:59 +0200

    Blocks are hopefully reconstructed

--- ordering/block_sparsity_pattern.py
+++ ordering/block_sparsity_pattern.py
@@ -1,6 +1,6 @@
 from __future__ import print_function
+from future_builtins import zip     # Python 2 only
 import numpy as np
-from itertools import izip
 
 # TODO 1. Reconstruct and somehow visualize blocks
 #      2. Do an ordering in the blocks, along the diagonal 
@@ -17,41 +17,55 @@ class BlockSparsityPattern:
         self.row_suffixes = { } # suffix name -> np.array of (index, value)
         self.col_suffixes = { } # suffix name -> np.array of (index, value)
 
-def stable_sort(partition):
-    # partition: (index, value), where value is the block id
-    # sort by block id
-    indices = np.argsort(partition, kind='mergesort', order='value')
-    for i in indices:
-        print('%d: %d' % (partition['value'][i], partition['index'][i]))
-    blk_ids = partition['value'][indices] # of length len(partition)==len(indices) 
-    print(blk_ids)
-    mask = np.ediff1d(blk_ids, to_begin=1, to_end=1) 
-    print(mask)
-    idx = np.flatnonzero(mask)
-    print(idx)
-    for i in xrange(idx.size-1):
-        print('%d: %s ' % (i, blk_ids[idx[i]:idx[i+1]]) )
-    p = partition['index'][indices]
-    for i in xrange(idx.size-1):
-        print('%d: %s ' % (i, p[idx[i]:idx[i+1]]) )
-    print([j for j in izip(idx, idx[1:])])
-    #return p, list of slices?
+def sort_by_block_id(partition):
+    # partition: array of (index, value) pairs, where value is the block id
+    # in place, stable sort by block id
+    np.ndarray.sort(partition, kind='mergesort', order='value')
 
-# FIXME Finish partition reconstruction from here
+def get_block_slices(partition):
+    # This function assumes that partition has already been sorted by block id 
+    # (by 'value'). Find the block boundaries first:
+    mask = np.ediff1d(partition['value'], to_begin=1, to_end=1)
+    idx = np.flatnonzero(mask) # adjancent elements of idx give the block slices 
+    return [slice(*t) for t in zip(idx[:-1],idx[1:])]
+
+def check_block_ids(partition, block_count):
+    # Assumption: block_ids == 1:block_count.
+    # block_ids is already sorted, and has block_count distinct elements.
+    block_ids = partition['value']
+    assert block_ids[ 0] == 1
+    assert block_ids[-1] == block_count
+
+def reconstruct(partition):
+    # returns: tuple of permutation vector, block boundaries (as slices)
+    sort_by_block_id(partition)
+    block_slices = get_block_slices(partition)
+    check_block_ids(partition, len(block_slices))
+    return partition['index'], block_slices
+
+def dbg_show(partition, permutation, block_slices):
+    print('permutation:', permutation)
+    print('blocks:')
+    for i, slc in enumerate(block_slices):
+        print(i, partition['value'][slc])
+    print('indices by block:')
+    for i, slc in enumerate(block_slices):
+        print(i, permutation[slc])        
+
 def reconstruct_partitions(bsp):
-    blk_id = 'blockid'
-    if (blk_id not in bsp.row_suffixes) or (blk_id not in bsp.col_suffixes):
+    blockid = 'blockid'
+    if (blockid not in bsp.row_suffixes) or (blockid not in bsp.col_suffixes):
         print('WARNING: No row and/or col partitions!')
         return
-    row_part, col_part = bsp.row_suffixes[blk_id], bsp.col_suffixes[blk_id]
-    # TODO Sanity checks: 1:nb, and all rows/columns have blk_id
-    print('rows:')
-    stable_sort(row_part)
-    print('cols:')
-    stable_sort(col_part)
-
-    #for k, g in groupby(row_part[i] for i in ind):
-    #    print(k, sorted(g))
-    quit()
-    return
+    row_partition = bsp.row_suffixes[blockid] 
+    col_partition = bsp.col_suffixes[blockid]
+    # Reconstruct sorts in place its argument
+    row_perm, row_blk_slices = reconstruct(row_partition) 
+    col_perm, col_blk_slices = reconstruct(col_partition)
+    assert len(row_blk_slices)==len(col_blk_slices)    
+    print('ROWS')
+    dbg_show(row_partition, row_perm, row_blk_slices)
+    print('COLS')
+    dbg_show(col_partition, col_perm, col_blk_slices)
+    return row_perm, row_blk_slices, col_perm, col_blk_slices
 
