776d3569653216681508d894eb48a43ee06a7cc6
baharev <ali.baharev@gmail.com>
2014-08-27 17:51:30 +0200

    Preparing to plot permuted matrix and blocks

--- ordering/sparse_plot.py
+++ ordering/sparse_plot.py
@@ -3,6 +3,38 @@ import scipy.sparse as sp
 import matplotlib.pyplot as plt
 import csr_utils as util
 
+def plot(bsp):
+    plot_matrix(bsp.csr_mat, bsp.row_names, bsp.col_names)
+
+def plot_matrix(m, row_names, col_names):
+    fig, ax = setup(m)
+    draw_nzeros(ax, m)
+    fs = get_font_size(fig, ax)
+    write_names(ax, row_names, col_names, fs) 
+    beautify_axes(ax)
+    plt.show()
+
+def setup(m):
+    assert isinstance(m, sp.csr_matrix)
+    fig=plt.figure()
+    ax=fig.add_subplot(111)
+    mng = plt.get_current_fig_manager()
+    mng.resize(1865,1025)
+    # TODO Post a wrapper to Code Review?
+    #mng.full_screen_toggle()     
+    plt.axis('scaled')
+    n_row = m.shape[0]
+    n_col = m.shape[1]    
+    ax.set_xlim([0, n_col])
+    ax.set_ylim([0, n_row])
+    return fig, ax
+
+def draw_nzeros(ax, m):
+    for i, j in util.itr_nonzero_indices(m):
+        # i and j must be swapped: row -> x axis, col -> y axis 
+        rect = plt.Rectangle((j, i), 1,1, facecolor='black',edgecolor='0.7')
+        ax.add_artist(rect)
+
 def get_font_size(fig, ax):
     SIZE = 72.0
     invisible = (1,1,1,0)
@@ -16,33 +48,14 @@ def get_font_size(fig, ax):
     print('approximate font size to set: {:.2f}'.format(SIZE/h))
     return 0.8*SIZE/h
 
-def plot_matrix(m, row_names, col_names):
-    assert isinstance(m, sp.csr_matrix)
-    fig=plt.figure()
-    ax=fig.add_subplot(111)
-    mng = plt.get_current_fig_manager()
-    mng.resize(1865,1025)
-    # TODO Post a wrapper to Code Review?
-    #mng.full_screen_toggle()     
-    plt.axis('scaled')
-    n_row = m.shape[0]
-    n_col = m.shape[1]    
-    ax.set_xlim([0, n_col])
-    ax.set_ylim([0, n_row])
-    for i, j in util.itr_nonzero_indices(m):
-        # i and j must be swapped: row -> x axis, col -> y axis 
-        rect = plt.Rectangle((j, i), 1,1, facecolor='black',edgecolor='0.7')
-        ax.add_artist(rect)
-    fs = get_font_size(fig, ax)
-    for r in xrange(n_row):
-        ax.text(-0.25, r+0.5, row_names[r], ha='right',  va='center', size=fs)
-    for c in xrange(n_col):
-        ax.text(c+0.5, -0.25, col_names[c], ha='center', va='bottom', size=fs, 
-                rotation=90)        
+def write_names(ax, row_names, col_names, fs):
+    for r, r_name in enumerate(row_names):
+        ax.text(-0.25, r+0.5,r_name,ha='right', va='center',size=fs)
+    for c, c_name in enumerate(col_names):
+        ax.text(c+0.5, -0.25,c_name,ha='center',va='bottom',size=fs,rotation=90)
+        
+def beautify_axes(ax):
     ax.invert_yaxis()
     ax.set_xticks([])
     ax.set_yticks([])
-    plt.show()
 
-def plot(bsp):
-    plot_matrix(bsp.csr_mat, bsp.row_names, bsp.col_names)
