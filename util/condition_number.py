f1259f004e2663b90618b2724c04c6af039dc1e2
baharev <ali.baharev@gmail.com>
2014-09-23 23:29:02 +0200

    Checking the condition number of the Jacobian at given points

---
+++ util/condition_number.py
@@ -0,0 +1,25 @@
+from __future__ import print_function
+import numpy as np
+from numpy.linalg import cond, norm
+from datagen.paths import DATADIR
+from os.path import join
+from parsers.dag_parser import read_problem
+from nodes.reverse_ad import prepare_evaluation_code
+from util.misc import import_code 
+
+def diagnostics(basename):
+    dagfilename = join(DATADIR, basename+'.dag')
+    prob = read_problem(dagfilename, plot_dag=False, show_sparsity=False)
+    partial_code = prepare_evaluation_code(prob) 
+    rev_ad = import_code(partial_code)
+    con, jac_ad = rev_ad.evaluate(prob.refsols[0], prob.ncons, prob.nvars, prob.nzeros)
+    print('Constraint infinity norm: ', norm(con, np.inf))
+    print('Condition number estimate:', cond(jac_ad.todense()))
+
+if __name__ == '__main__':
+    diagnostics('LLE3rev')
+    diagnostics('LLE3revtriv')    
+    
+     
+    
+
