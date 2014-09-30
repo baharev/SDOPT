173e5223e83951ebfa40c3b24283c3598f619da3
baharev <ali.baharev@gmail.com>
2014-09-30 17:03:04 +0200

    Cleaning up before attempting make a distribution

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from __future__ import print_function
 from parsers.dag_parser import read_problem
+from os.path import join
 
 if __name__ == '__main__':
-    read_problem('./data/JacobsenDbg.dag')
+    read_problem(join('data', 'JacobsenDbg.dag'))
     print()
-    read_problem('./data/ex9_2_8.txt', crosscheck_nl=False)
+    read_problem(join('data', 'ex9_2_8.txt'), crosscheck_nl=False)
