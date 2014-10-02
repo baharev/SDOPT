1c72cb2946a1aac1487ca75d0bb1d3333a7d12a6
baharev <ali.baharev@gmail.com>
2014-10-03 01:40:46 +0200

    Mocking out numpy, scipy, networkx imports hopefully fixes build error on readthedocs.org

--- docs/conf.py
+++ docs/conf.py
@@ -114,6 +114,15 @@ else:
     html_theme = "sphinx_rtd_theme"
     html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
+import mock
+ 
+MOCK_MODULES = ['numpy', 'scipy', 'scipy.sparse', 'networkx', 
+                'networkx.algorithms.dag', 'networkx.algorithms'
+               ]
+
+for mod_name in MOCK_MODULES:
+    sys.modules[mod_name] = mock.Mock()
+
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
