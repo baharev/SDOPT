bdfb354bd8e149f43f43bb40d49dbae733e48e73
baharev <ali.baharev@gmail.com>
2014-10-02 22:49:51 +0200

    Updating path after moving the directories around

--- sdopt/datagen/paths.py
+++ sdopt/datagen/paths.py
@@ -1,8 +1,8 @@
 from tempfile import gettempdir
 from os.path import join
-from util.rootpkg_path import rootpkg_path
+from sdopt import _ROOT
 
 TMPDIR  = join(gettempdir(), 'gjh') # /tmp/gjh on Linux
-DATADIR = join(rootpkg_path, 'data')
+DATADIR = join(_ROOT, 'datagen', 'data')
 AMPL = 'ampl' # <- Keep in sync with convert.sh
-CONVERTER = rootpkg_path+'/datagen/convert.sh' # Won't work on Win anyway
+CONVERTER = _ROOT+'/datagen/convert.sh' # Won't work on Win anyway
--- sdopt/util/rootpkg_path.py
+++
@@ -1,5 +0,0 @@
-from os.path import dirname, sep
-
-_module_path = dirname(__file__)
-rootpkg_path = _module_path[:_module_path.rfind(sep)]
-
