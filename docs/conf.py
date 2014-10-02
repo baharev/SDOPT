9d20ffef8f99260d1458525dc6644a6827164d7a
baharev <ali.baharev@gmail.com>
2014-10-03 00:27:15 +0200

    A horrible hack: putting sdopt on the sys.path; must be fixed

--- docs/conf.py
+++ docs/conf.py
@@ -19,8 +19,6 @@ import os
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 sys.path.insert(0, os.path.abspath('..'))
-#FIXME 
-sys.path.insert(0, os.path.abspath(os.path.join('..','sdopt')))
 
 # -- General configuration ------------------------------------------------
 
--- sdopt/__init__.py
+++ sdopt/__init__.py
@@ -1,3 +1,7 @@
 import os
+import sys 
 
 _ROOT = os.path.abspath(os.path.dirname(__file__))
+
+#FIXME Horrible hack
+sys.path.insert(0, _ROOT)
