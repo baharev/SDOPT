221c68338293c35311dd68d7bed6d20bbe1095e8
baharev <ali.baharev@gmail.com>
2014-10-04 14:50:45 +0200

    AutoNumber, derived from Enum gave all sorts of weird "Error when calling the metaclass bases" errors, crashing sphinx-autodoc. Interestingly, the test also run 25% faster now that I replaces AutoNumber with manually written enum (hard-coded ints)

--- sdopt/nodes/attributes.py
+++ sdopt/nodes/attributes.py
@@ -1,21 +1,24 @@
 from collections import namedtuple
-from ..util.autoenum import AutoNumber
 
 class Bounds(namedtuple('Bounds', 'l u')):
         __slots__ = ()
         def __str__(self):
             return '[%g,%g]' % (self.l, self.u)
 
-class NodeAttr(AutoNumber):
-    bounds    = ()
-    con_num   = ()
-    d_term    = ()
-    display   = ()
-    input_ord = ()
-    n_term    = ()
-    name      = ()
-    node_id   = ()
-    number    = ()
-    operation = ()
-    type      = ()
-    var_num   = ()
+# AutoNumber, derived from Enum gave all sorts of weird "Error when calling the 
+# metaclass bases" errors, crashing the problem. Interestingly, the test also 
+# run 25% faster now...
+
+class NodeAttr:
+    bounds    =  1
+    con_num   =  2
+    d_term    =  3
+    display   =  4
+    input_ord =  5
+    n_term    =  6
+    name      =  7
+    node_id   =  8
+    number    =  9
+    operation = 10
+    type      = 11
+    var_num   = 12
